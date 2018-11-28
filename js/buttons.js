fa4669386883dc261f5974098b034705c96d86cd
Emilyf <emilygracefinch@outlook.com>
2018-11-28 17:14:39 -0500

    fpp

--- js/buttons.js
+++ js/buttons.js
@@ -1,3 +1,3 @@
 document.getElementById("buttons").onclick = function() {
-    window.location.href = <a href="https://emifinch.github.io/duringjava/experience.html#foo">Jump to #foo on Experience page</a>"
+    window.location.href ="https://emifinch.github.io/duringjava/experience.html#foo"
   };
