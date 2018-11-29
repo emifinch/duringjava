b319cf83dc4f4f1a0ef75a7802fe73d71756a7da
Emilyf <emilygracefinch@outlook.com>
2018-11-29 16:42:02 -0500

    adf

--- js/buttons.js
+++ js/buttons.js
@@ -3,6 +3,21 @@ function legal() {
   window.location.href ="https://emifinch.github.io/duringjava/experience.html#foo"
 }
 
+function museum() {
+  console.log('Hey');
+  window.location.href ="https://emifinch.github.io/duringjava/experience.html#oof"
+}
+
+function other() {
+  console.log('Hey');
+  window.location.href ="https://emifinch.github.io/duringjava/experience.html#off"
+}
+
+function library() {
+  console.log('Hey');
+  window.location.href ="https://emifinch.github.io/duringjava/experience.html#ofo"
+}
+
   // document.getElementById("legalbutton").onclick = function() {
   //   console.log('Hey');
   //     window.location.href ="https://emifinch.github.io/duringjava/experience.html#foo"
