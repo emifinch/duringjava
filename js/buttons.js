9ec9ade3d1c4b3e6e93b11a696b4dab5b9b33469
Emilyf <emilygracefinch@outlook.com>
2018-11-29 16:25:42 -0500

    sdf

--- js/buttons.js
+++ js/buttons.js
@@ -1,5 +1,5 @@
 document.getElementById("buttons").onclick = function() {
-    window.location.href ="https://emifinch.github.io/duringjava/experience.html"
+    window.location.href ="https://emifinch.github.io/duringjava/experience.html#foo"
   };
 
 // $('#some_id').click(function() {
