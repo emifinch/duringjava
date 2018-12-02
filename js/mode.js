66839daf90041b96dc4c6e0d0e51413e07253ffa
Emilyf <emilygracefinch@outlook.com>
2018-12-02 13:36:26 -0500

    lets try this one

--- js/mode.js
+++ js/mode.js
@@ -1,5 +1,17 @@
-$('#overlay').modal('show');
+var modal = document.querySelector(".modal");
+    var trigger = document.querySelector(".trigger");
+    var closeButton = document.querySelector(".close-button");
 
-setTimeout(function() {
-    $('#overlay').modal('hide');
-}, 5000);
+    function toggleModal() {
+        modal.classList.toggle("show-modal");
+    }
+
+    function windowOnClick(event) {
+        if (event.target === modal) {
+            toggleModal();
+        }
+    }
+
+    trigger.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+    window.addEventListener("click", windowOnClick);
